0226a268d18ff2e1ea2b06854b3a9798bbdf0eb1
JongTTing <ejh990521@gmail.com>
2023-03-24 22:02:42 +0900

    2장

---
+++ toby-spring/src/main/java/org/toby/user/dao/DaoFactory.java
@@ -0,0 +1,30 @@
+package org.toby.user.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DaoFactory {
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/tobi?characterEncoding=UTF-8");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+
+        return dataSource;
+    }
+
+    @Bean
+    public UserDao userDao() {
+        UserDao userDao = new UserDao();
+        userDao.setDataSource(dataSource());
+        return userDao;
+    }
+
+}
